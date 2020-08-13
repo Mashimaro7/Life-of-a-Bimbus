61236dc65badbc7f8c01c83f8681a6501653d088
Mashimaro7 <63817298+Mashimaro7@users.noreply.github.com>
2020-08-13 16:42:26 +0700

    0.13

--- Assets/Scripts/Bimbus Scripts/BimbusMove.cs
+++ Assets/Scripts/Bimbus Scripts/BimbusMove.cs
@@ -38,6 +38,7 @@ public class BimbusMove : MonoBehaviour
     {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         if (speed > 0.2f)
         {
             moving = true;
@@ -52,6 +53,9 @@ public class BimbusMove : MonoBehaviour
 =======
         
 >>>>>>> parent of 99ad2c4... 0.13
+=======
+        
+>>>>>>> parent of 99ad2c4... 0.13
         if (!stats.isDead)
         {
             speed = navAgent.velocity.magnitude / navAgent.speed;
@@ -62,8 +66,11 @@ public class BimbusMove : MonoBehaviour
     {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         step.Play();
 =======
+=======
+>>>>>>> parent of 99ad2c4... 0.13
 =======
 >>>>>>> parent of 99ad2c4... 0.13
 
@@ -73,7 +80,10 @@ public class BimbusMove : MonoBehaviour
         Quaternion.RotateTowards(transformo.rotation, transformo.rotation,step);
         rising = false;
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 7a32e9a78d2c5431339cdcc3544d2ef483840b79
+=======
+>>>>>>> parent of 99ad2c4... 0.13
 =======
 >>>>>>> parent of 99ad2c4... 0.13
     }
